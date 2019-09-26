5a970e45641b16c6fd7f6d7bca28db6a6925587b
Taylor Covington <tcovingtondev@gmail.com>
2019-09-25 18:17:57 -0600

    rails done for now

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,37 @@
+class Api::PostsController < ApplicationController
+  # only have to worry about the CRUD 
+
+  def index
+    render json: Post.all
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      render json: @post
+    else
+      render json: { errors: @post.erros }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.updates(post_params)
+      render json: @post
+    else
+      render json: { errors: @post.erros }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    Post.find(params[:id]).destroy
+    render json: { message: 'post deleted' }
+  end
+
+  private
+    def post_params
+      # { post: {title: '', body: ''}}  this is what is being permitted
+       params.require(:post).permit(:title, :body) 
+    end
+
+end
